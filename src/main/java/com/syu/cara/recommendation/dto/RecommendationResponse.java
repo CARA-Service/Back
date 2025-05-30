6c665a3534743aeaa4380f8cc7b06857981ff2ae
minseong <minseong0213@naver.com>
2025-05-30 16:38:51 +0900

    fix : RecommendationRequestDTO & RecommendationResponseDTO modify

--- src/main/java/com/syu/cara/recommendation/dto/RecommendationResponse.java
+++ src/main/java/com/syu/cara/recommendation/dto/RecommendationResponse.java
@@ -1,9 +1,21 @@
 package com.syu.cara.recommendation.dto;
 
+import lombok.Data;
+
+@Data
 public class RecommendationResponse {
     private Long recommendationId;
     private Long carId;
+    private String modelName;
+
+    private String fuelType;
+    private double fuelEfficiency;
+    private int capacity;
+    private String luggageSize;
+
     private int totalPrice;
     private int basePrice;
+
     private String agencyName;
+
 }

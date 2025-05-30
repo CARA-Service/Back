67777ae6bf124de7e82936b04e63fb9324699630
유민수 <ryu@yuminsuui-MacBookPro.local>
2025-05-30 14:11:30 +0900

    feat : CarDetailResponse DTO

---
+++ src/main/java/com/syu/cara/car/dto/CarDetailResponse.java
@@ -0,0 +1,9 @@
+package com.syu.cara.car.dto;
+
+public class CarDetailResponse {
+    private Long carId;
+    private String modelName;
+    private String manufacturer;
+    private Double fuelEfficiency;
+    private String additionalOptions;
+}

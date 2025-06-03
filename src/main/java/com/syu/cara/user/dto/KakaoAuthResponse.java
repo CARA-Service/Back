f9b912f601961c9473527fa330696555473dff0b
유민수 <ryu@yuminsuui-MacBookPro.local>
2025-06-03 14:45:36 +0900

    fix : KakaoAuthResponse DTO lombok import

--- src/main/java/com/syu/cara/user/dto/KakaoAuthResponse.java
+++ src/main/java/com/syu/cara/user/dto/KakaoAuthResponse.java
@@ -1,7 +1,13 @@
 package com.syu.cara.user.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class KakaoAuthResponse {
     private Long userId;
     private String loginId;
     private boolean isNewUser;
+    private String jwtToken;
 }

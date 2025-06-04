63cfd6cff91fd059ef379652f0de840574d346e9
유민수 <ryu@yuminsuui-MacBookPro.local>
2025-06-04 21:49:18 +0900

    fix : isNewUser 직렬화

--- src/main/java/com/syu/cara/user/dto/KakaoAuthResponse.java
+++ src/main/java/com/syu/cara/user/dto/KakaoAuthResponse.java
@@ -1,5 +1,6 @@
 package com.syu.cara.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,6 +9,10 @@ import lombok.Data;
 public class KakaoAuthResponse {
     private Long userId;
     private String loginId;
+
+    // "isNewUser"라는 이름으로 직렬화
+    @JsonProperty("isNewUser")
     private boolean isNewUser;
+
     private String jwtToken;
 }

c10e366df37bc005012835a1fb341a13499e4490
유민수 <ryu@yuminsuui-MacBookPro.local>
2025-06-10 16:58:18 +0900

    feat : UserProfileResponse DTO

---
+++ src/main/java/com/syu/cara/user/dto/UserProfileResponse.java
@@ -0,0 +1,15 @@
+package com.syu.cara.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserProfileResponse {
+    private String loginId;
+    private String email;
+    private String fullName;
+    private String profileImageUrl;
+    private String driver_license;
+    private String address;
+}

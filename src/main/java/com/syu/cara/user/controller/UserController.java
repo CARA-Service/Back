3592a1aedaa2c677b2aa758775268b1213bb5a36
유민수 <ryu@yuminsuui-MacBookPro.local>
2025-06-10 17:09:53 +0900

    feat : UserProfileService Controller

---
+++ src/main/java/com/syu/cara/user/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.syu.cara.user.controller;
+
+import com.syu.cara.user.dto.UserProfileResponse;
+import com.syu.cara.user.service.UserProfileService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserProfileService profileService;
+
+    public UserController(UserProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserProfileResponse> getMyProfile() {
+        return ResponseEntity.ok(profileService.getMyProfile());
+    }
+}

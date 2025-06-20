589d6d7c053064b19703523cc6c3ecf70e545f82
유민수 <ryu@yuminsuui-MacBookPro.local>
2025-06-20 13:36:22 +0900

    feat : Authentication, PasswordEncoder Bean

--- src/main/java/com/syu/cara/config/SecurityConfig.java
+++ src/main/java/com/syu/cara/config/SecurityConfig.java
@@ -6,9 +6,13 @@ import com.syu.cara.user.service.CustomUserDetailsService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -48,4 +52,19 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    return http.getSharedObject(AuthenticationManagerBuilder.class)
+        .userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder())
+        .and()
+        .build();
+}
+
+@Bean
+public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}
+
 }

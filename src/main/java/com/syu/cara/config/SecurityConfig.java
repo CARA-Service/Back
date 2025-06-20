8210328292b23bfd71a3c72ca5c60740692b8798
유민수 <ryu@yuminsuui-MacBookPro.local>
2025-06-20 15:50:21 +0900

    feat : test

--- src/main/java/com/syu/cara/config/SecurityConfig.java
+++ src/main/java/com/syu/cara/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/api/v1/auth/kakao",
+                    "/api/v1/auth/signup",
                     "/api/v1/auth/login",
                     "/api/v1/auth/logout",
                     "/api/v1/auth/kakao/callback"
